2796cfdf24228ef2130db519c1b22c2be8fa3be9
Houseliofer <20030392@itcelaya.edu.mx>
2023-05-18 20:33:29 -0600

    Agregada funcionalidad de fecha en index.js

--- js/components/modal.js
+++ js/components/modal.js
@@ -11,12 +11,7 @@ export default class Modal {
     this.todo = null;
   }
 
-  setValues(todo) {
-    this.todo = todo;
-    this.title.value = todo.title;
-    this.description.value = todo.description;
-    this.completed.checked = todo.completed;
-  }
+
 
   onClick(callback) {
     this.btn.onclick = () => {
